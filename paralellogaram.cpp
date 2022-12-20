bff53c9ec3cc72bf884bd590cbead23f432485d9
Hp <Sami03136424477@gmail.com>
2022-12-20 20:04:48 +0500

    message

---
+++ paralellogaram.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+cout<<"                                 ________________________________________________   "<<endl;
+cout<<"                                $                                                $  "<<endl;                                                      
+cout<<"                               $                                                $   "<<endl;                                                      
+cout<<"                              $                                                $    "<<endl;                                                      
+cout<<"                             $                                                $     "<<endl;                                                      
+cout<<"                            $                                                $      "<<endl;                                                      
+cout<<"                           $                                                $       "<<endl;                                                      
+cout<<"                          $                                                $        "<<endl;                                                      
+cout<<"                         $                                                $         "<<endl;                                                      
+cout<<"                        $                                                $          "<<endl;
+cout<<"                       $                                                $           "<<endl;                                                     
+cout<<"                      $                                                $            "<<endl;
+cout<<"                     $                                                $             "<<endl;
+cout<<"                    $_______________________________________________ $              "<<endl;
+
+
+
+}
---
+++ task5 a(2).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"      *****************************************                                                              "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *     Universuty Addmission             *                       "<<endl;
+cout<<"      *     Mangment System                   *                       "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *                                       *                       "<<endl;
+cout<<"      *****************************************                                                              "<<endl;
+}
